3a28fdc4aa532619684c571aa93e930856f9d156
Arbitrarion <arbitrarion@gmail.com>
2017-04-04 00:07:18 -0700

    added -m option
    -m writes to memory instead of file

--- catable.py
+++ catable.py
@@ -3,30 +3,82 @@
 import sys;
 import os;
 
-#get arg
-filearg = sys.argv[1]
+#global vars
+false=bool(0)
+true=bool(1)
 
+fileArg = ''
 wrappedFileName = 'output.txt'
 newlineCharacters = ['\n']
+memVar = 'qwymzxxcovi'
+printToMemory = false
+
+############################function definitions
+def parseArgs():
+	if(len(sys.argv) < 2):
+		return false
+	global fileArg
+	fileArg = sys.argv[1]
+	for i in range(2, len(sys.argv)):
+		if (sys.argv[i] == '-m'):
+			print "write to memory enabled"
+			global printToMemory
+			printToMemory = true;
+
+	return true
 
 
 
+def escapeString(line):
+	escapedLine = ""
+	for char in line:
+		if(not char in newlineCharacters):
+			escapedLine += '\\' + char
+	return escapedLine
 
+def printHeader(outfile):
+	if(printToMemory):
+		outfile.write(memVar + "=''\n")
+	else:
+		outfile.write('rm ' + wrappedFileName + '; touch ' + wrappedFileName + '\n')
 
-if(not filearg):
-	print "Usage: catable.py <file>"
+		#ifelse skasofinA: add file check, abort if not exists
+		outfile.write('if [[ ! -a ' + wrappedFileName + ' ]]; then\n')
+		outfile.write('\techo "Error writing to file, trying regenerating with the -m flag."\n')
+		outfile.write('else\n')
+		
+def printLine(outfile, escapedLine):
+	if(printToMemory):
+		toPrint = memVar + '=$' + memVar + '\ ' + escapedLine + '\n'
+		outfile.write(toPrint)
+	else:
+		outfile.write('\techo ' + escapedLine + ' >> ' + wrappedFileName + '\n')
+
+def printFooter(outfile):
+	if(printToMemory):
+		outfile.write('echo "file written to variable \\$' + memVar + '."\n')
+	else:
+		#endif skasofinA
+		outfile.write('fi\n')
+	print 'File has been catabled, check it out at ./catme.sh'
+
+###########################main
+
+if(not parseArgs()):
+	print "Usage: catable.py <file> [options]"
 	print "catable takes a file and writes a file (catme.sh) that print the original file to " + wrappedFileName + "."
 	print "This is useful if you have access to a remote terminal, but cannot upload a simple file."
+	print "Options:"
+	print "-m\tWrite file to memory(useful when you can't write to disk on target)"
+
 else:
-
-	infile = open(filearg,'r');
+	infile = open(fileArg,'r');
 	outfile = open('catme.sh','w');
-	outfile.write('rm ' + wrappedFileName + '; touch ' + wrappedFileName + '\n')
-	for line in infile:
-		escapedLine = ""
-		for char in line:
-			if(not char in newlineCharacters):
-				escapedLine += '\\' + char
 
-		outfile.write('echo ' + escapedLine + ' >> ' + wrappedFileName + ' \n')
-	print 'File has been catabled, check it out at ./catme.sh'
+	printHeader(outfile)
+	#echo wrap file contents
+	for line in infile:
+		printLine(outfile, escapeString(line))
+		
+	printFooter(outfile)
+
